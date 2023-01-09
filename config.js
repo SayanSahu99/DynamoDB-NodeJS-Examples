561bc104e927446bf6b192ca627bb2c4f96d96b2
SayanSahu99 <sayansovan@gmail.com>
2023-01-09 14:50:28 +0530

    AWS config

---
+++ config.js
@@ -0,0 +1,8 @@
+const config = {
+    endpoint: "http://localhost:8000", 
+    accessKeyId: 'fakeMyKeyId', 
+    secretAccessKey: 'fakeSecretAccessKey', 
+    region: "ap-south-1"
+}
+
+module.exports = config;
